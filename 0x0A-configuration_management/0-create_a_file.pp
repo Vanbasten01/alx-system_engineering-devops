30312d056892c64b52eb41c25bb771000d0da743
Vanbasten01 <fouad.yasin01@gmail.com>
2023-09-22 16:14:12 +0100

    Puppet manifest to create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # puppet manifest that create a file in /tmp
 
 file { '/tmp/school':
-  ensure => 'file'
-  path => '/tmp/school',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
